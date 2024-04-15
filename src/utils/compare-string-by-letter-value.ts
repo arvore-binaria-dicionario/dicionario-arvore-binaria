6e96e79a24913d4e124c0b4495f551b493300d8b
habini86 <vinicius1234585@gmail.com>
2024-04-15 13:21:38 -0300

    refactor: retirando os espaços das strings

--- src/utils/compare-string-by-letter-value.ts
+++ src/utils/compare-string-by-letter-value.ts
@@ -1,13 +1,26 @@
 type Insert = 'left' | 'right'
 
 export default function (rootValues: string, compare: string): Insert {
-  const result: number = rootValues.localeCompare(compare, undefined, {
-    sensitivity: 'base',
-  })
+  const rootWithoutSpaces = removeAllSpaces(rootValues).toLowerCase()
+  const compareWithoutSpaces = removeAllSpaces(compare).toLowerCase()
+
+  console.log(compareWithoutSpaces)
+
+  const result: number = rootWithoutSpaces.localeCompare(
+    compareWithoutSpaces,
+    undefined,
+    {
+      sensitivity: 'base',
+    },
+  )
 
   if (result === 1 || result === 0) {
     return 'left'
   }
 
   return 'right'
+}
+
+function removeAllSpaces(str: string) {
+  return str.split(' ').join('')
 }
