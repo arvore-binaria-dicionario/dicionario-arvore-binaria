0f7836bc0882c40f4f97c232525df461deabc6f5
habini86 <vinicius1234585@gmail.com>
2024-05-06 12:00:47 -0300

    init: iniciando projeto front-end

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { BinaryTree } from './utils/interface-arvore'
 import path from 'path'
 import csvToBinaryTree from './utils/csvToBinaryTree'
+import { app, BrowserWindow } from 'electron'
+
 const csvFilePath = path.resolve(__dirname, './csv/dicionario.csv')
 
 const dictionary = new BinaryTree()
@@ -17,3 +19,35 @@ async function main() {
 }
 
 main()
+
+let mainWindow: Electron.BrowserWindow | null
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  })
+
+  mainWindow.loadFile('index.html')
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
